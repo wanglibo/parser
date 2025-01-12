e96540e62656fcaf6098013bf56807a6df81a370
Libo Wang <billikx@gmail.com>
2025-01-11 22:46:13 -0800

    support number validation and simplified some char lookup

--- test.py
+++ test.py
@@ -1,14 +1,16 @@
 from token import Token
 from typing import List
 
+SINGLE_CHAR_OPERATORS='.,;=()+-*/%'
+
 def isSymbol(char: str) -> bool:
-    return char in ['.', ',', ';', '=', '>', '<', '!', '(', ')', '+', '-', '*', '/', '%']
+    return char in SINGLE_CHAR_OPERATORS or char in '><!'
 
 
 def matchOperator(s: str, position: int) -> str:
-    if s[position] in ['.', ',', ';', '=', '(', ')', '+', '-', '*', '/', '%']:
+    if s[position] in SINGLE_CHAR_OPERATORS:
         return s[position]
-    elif s[position] == '>' or s[position] == '<' or s[position] == '!':
+    elif s[position] in '><!':
         if position+1 >= len(s) or s[position+1] != '=':
             return s[position]
         # >=, <=, !=
@@ -35,9 +37,27 @@ def tokenize(query: str) -> List[str]:
         # This does not handle special cases like hex (0x62af), negative numbers and floats yet.
         elif query[ptr].isdigit():
             start = ptr
-            while ptr < len(query) and query[ptr].isdigit():
+            while ptr < len(query) and (query[ptr].isdigit() or query[ptr] in '.abcdefABCDEFx'):
                 ptr += 1
             segment = query[start:ptr]
+            valid = False
+            try:
+                int(segment)
+                valid = True
+            except ValueError:
+                pass
+            try: 
+                int(segment, 16)
+                valid = True
+            except ValueError:
+                pass
+            try:
+                float(segment)
+                valid = True
+            except ValueError:
+                pass
+            if not valid:
+                raise ValueError('Invalid number in query: ', segment)
             tokens.append(Token(segment))
         # Check if the current character is a symbol
         # This could be problematic, if we have a multi-character symbol like '!=', '<=', '>=', etc.
@@ -52,7 +72,7 @@ def tokenize(query: str) -> List[str]:
     return tokens
 
 def test():
-    query = "SELECT a.b FROM table WHERE column <= 3 and column2 < 1;"
+    query = "SELECT a.b FROM table WHERE column <= 3.1 and column2 < 0x1a;"
     print(query)
     tokens = tokenize(query)
     print(tokens)
